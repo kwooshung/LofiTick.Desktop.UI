0867d23cab1b4445192816845d3cff00e7723df6
kwooshung <kwooshung@qq.com>
2026-04-11 14:07:08 +0800

    ✨ feat(settings): add settings page with navigation menu and localization

---
+++ app/pages/settings.vue
@@ -0,0 +1,56 @@
+<template>
+  <Dashboard>
+    <template #toolbar-left>
+      <UNavigationMenu :items="links" highlight class="-translate-x-2.5" />
+    </template>
+    <NuxtPage />
+  </Dashboard>
+</template>
+
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui';
+
+/**
+ * Hook：国际化
+ */
+const { t } = useI18n();
+
+/**
+ * 函数：本地化路由
+ */
+const localePath = useLocalePath();
+
+/**
+ * 变量：设置页 Tab 链接
+ */
+const links = [
+  [
+    {
+      label: t('pages.settings.general.title'),
+      icon: 'i-proicons:settings',
+      to: localePath('/settings'),
+      exact: true
+    },
+    {
+      label: t('pages.settings.ui.title'),
+      icon: 'i-lucide:brush-cleaning',
+      to: localePath('/settings/ui')
+    },
+    {
+      label: t('pages.settings.playback.title'),
+      icon: 'i-lucide:play',
+      to: localePath('/settings/playback')
+    },
+    {
+      label: t('pages.settings.power.title'),
+      icon: 'i-carbon:flash',
+      to: localePath('/settings/power')
+    },
+    {
+      label: t('pages.settings.unattended.title'),
+      icon: 'i-mdi:robot-outline',
+      to: localePath('/settings/unattended')
+    }
+  ]
+] satisfies NavigationMenuItem[][];
+</script>
